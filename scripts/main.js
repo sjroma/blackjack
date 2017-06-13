fc18ea30186e1b1c68724949e7e631195ce5c435
Steve Roma <sjroma@hotmail.com>
2017-06-13 13:31:34 -0400

    push solution to Newlines Blackjack Hand Calculator project

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+/*
+   Implement a Blackjack hand value calculator.
+
+   Open up the `index.html` file and your console
+   to watch the assertions pass as you write your code.
+
+   Also remember, that the parameter `hand` will be an array, so
+   you'll need to parse through that first before you can start to 
+   write your logic.
+*/
+
+function handValue (hand) {
+  for (i = 0; i < hand.length; i++){
+    if ((hand[i] == "K") || (hand[i] == "Q") || (hand[i] == "J")){
+
+      hand[i] = 10;
+
+    } else if (hand[i] == "A"){
+      hand[i] = 11;
+
+    } else {
+      hand[i] = parseInt(hand[i]);
+    }
+  }
+
+  let sum = hand.reduce((a, b) => a + b, 0);
+
+  if (sum > 21){
+    for (i = 0; i < hand.length; i++){
+      if (hand[i] == 11){
+        hand[i] = hand[i] - 10;
+
+      }
+    }
+  }
+
+  sum = hand.reduce((a, b) => a + b, 0);
+  return sum;
+}
+
+
+/* -----  Hints ------
+
+1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
+K, Q, J ==> Worth 10
+A       ==> Worth 1 or 11
+
+*/
